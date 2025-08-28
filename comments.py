2b23b8a5d9cf639c44635fa6eab85d8de1a45d65
Sneha <biradarsneha1245@gmail.com>
2025-08-28 13:36:59 +0530

    comments

---
+++ comments.py
@@ -0,0 +1,11 @@
+#This is a single line comment
+print("good day")
+print("welldone")#printing welldone
+#print("it's ok")
+print("next time")
+
+
+
+'''This is multiline comment statement'''
+"""This is also use for multiline comment"""
+print("succesfully completed")
