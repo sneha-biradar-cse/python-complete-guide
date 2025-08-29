7b2a13c956cc06410e9f56a0beec45e2da7b3032
Sneha <biradarsneha1245@gmail.com>
2025-08-29 21:45:18 +0530

    escape sequence

---
+++ escapesequences&printstatement.py
@@ -0,0 +1,10 @@
+#escape sequence
+print("Hey I an a good boy\nand this viewer is also a good boy/girl")
+print("Hey I am a \"good boy\"\nand this viewer is also a goo boy/girl")
+
+#print statement
+print("Hey",6,7,sep="~",end="009")
+print("Harry")
+
+print("Hey",6,7,sep="~",end="009\n")
+print("Harry")
