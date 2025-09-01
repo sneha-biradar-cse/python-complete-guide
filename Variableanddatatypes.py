e82b3e03cb418d6941cee1375cd39f9054987ea8
Sneha <biradarsneha1245@gmail.com>
2025-09-02 00:30:12 +0530

    variable and data types

---
+++ Variableanddatatypes.py
@@ -0,0 +1,15 @@
+a=1            #Integer variable
+print(a)
+
+b="hello"     #string variable
+print(b)
+
+c=123 
+d=9
+print(c+d)
+
+print("Type of a is",type(a))
+print("Type of a is",type(b))
+
+lists=[8,2.3,[-4,5,["apple","grapes"]]]
+print(lists)
