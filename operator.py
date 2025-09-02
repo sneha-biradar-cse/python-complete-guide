7643fe5cbc99181afd2b2850e389c83cb1bff69d
Sneha <biradarsneha1245@gmail.com>
2025-09-03 00:32:49 +0530

    operator

---
+++ operator.py
@@ -0,0 +1,7 @@
+print(16+4)      #addition
+print(16-4)      #subtraction
+print(16*4)      #multiplication
+print(16/4)      #division
+print(16//4)     #floor division
+print(16%4)      #modulus
+print(16**4)     #exponential
