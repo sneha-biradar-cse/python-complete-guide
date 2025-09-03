823bee93bed66505b69e5297bea163a9e0edf3bb
Sneha <biradarsneha1245@gmail.com>
2025-09-04 00:12:45 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,8 @@
+#calculator using python
+a=50
+b=3
+
+print("The value of", a, "+",3,"is:",a+b)
+print("The value of", a, "-",3,"is:",a-b)
+print("The value of", a, "*",3,"is:",a*b)
+print("The value of", a, "/",3,"is:",a/b)
