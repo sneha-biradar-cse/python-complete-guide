5e2e55e1e1a71d1a3c67d9488a109be5eefb97f2
Sneha <biradarsneha1245@gmail.com>
2025-09-07 22:10:29 +0530

    taking user input

---
+++ takinguserinput.py
@@ -0,0 +1,12 @@
+#Taking User Input
+
+a=input()
+print(a)
+
+a=input()
+print("My name is", a)
+
+a=input("enter a number:")  #a=2
+b=input("enter a number:")  #b=4
+print(a+b)                  #output=24
+print(int(a)+int(b))        #output=6
