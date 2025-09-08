d640d04a8a3d883e7b9731f7c7472f4e6d15e5bf
Sneha <biradarsneha1245@gmail.com>
2025-09-08 23:58:41 +0530

    string

---
+++ Srting.py
@@ -0,0 +1,25 @@
+#strings in python
+
+name="Dora"
+print("hello"+name)
+
+#print("this is very "unrealistic" situation")  #Error occur
+print('this is very "unrealistic" situation')
+print("this is very \"unrealistic\"situation")
+
+#multiline string
+A='''hey
+I am good
+what about you'''
+print(A)
+
+B="""hey
+I am good
+what about you"""
+print(B)
+
+#string Indexing
+c="Nobita"
+print(c[0])
+print(c[1])
+print(c[2])
