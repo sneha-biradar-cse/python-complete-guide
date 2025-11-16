12c9b27efbce55fcefd8d39c1d85f2c75a3dfe97
Sneha <biradarsneha1245@gmail.com>
2025-11-16 18:02:33 +0530

    operator

--- typecasting.py
+++ typecasting.py
@@ -3,12 +3,12 @@
 #Explicit Typecasting
 a="1"
 b="2"
-print(int(a)+int(b))
+print(int(a)+int(b))   #output=3
 
 
 #Implicit Typecasting
 c=1.9
 d=4
-print(c+d)
+print(c+d)            #output=5.9
 
-print(type(c+d))
+print(type(c+d))      #output=<class 'float'>
