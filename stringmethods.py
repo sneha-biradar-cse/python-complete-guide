af8107bc8ceccccecbc1943fa9aed5c31919cb08
Sneha <biradarsneha1245@gmail.com>
2025-11-24 21:31:07 +0530

    string method

---
+++ stringmethods.py
@@ -0,0 +1,44 @@
+a="Dora!!!  doe"
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("doe","man"))
+print(a.split("  "))
+heading="introductin to python"
+print(heading.capitalize())
+
+str1 = "Welcome all"
+print(len(str1))
+print(len(str1.center(22)))
+print(a.count("Dora"))
+
+str1 = "Welcome all"
+print(str1.endswith("all"))
+print(str1.endswith("all", 8, 11))
+print(str1.find("come"))
+
+str2 = "Welcome all"
+print(str2.isalnum())
+str2 = "Welcome"
+print(str2.isalpha())
+str2 = "Welcome all"
+print(str2.islower())
+
+str3="hello all\n"
+print(str3.isprintable())
+
+str3 = "   "
+print(str3.isspace())
+
+str1 = "Welcome all"
+print(str1.istitle())
+
+str1 = "welcome all"
+print(str1.startswith("welcome"))
+
+str1 = "welcome all"
+print(str1.swapcase())
+
+str1 = "welcome all"
+print(str1.title())
