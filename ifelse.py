b60ebcc37fca5df95cea70462290e5be4e610198
Sneha <biradarsneha1245@gmail.com>
2025-12-14 11:24:56 +0530

    ifelse

---
+++ ifelse.py
@@ -0,0 +1,9 @@
+#ifelse statement
+applePrice = 210
+budget = 200
+if(applePrice <= budget):
+    print("Alexa, add 1kg apples to the cart.")
+else:
+     print("Alexa,  do not add apples to the cart.") 
+
+
