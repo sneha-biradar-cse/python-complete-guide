73212e9d4ca618503b34081bbfffafbddd35a0d7
Sneha <biradarsneha1245@gmail.com>
2026-01-30 00:17:20 +0530

    forloop

---
+++ Forloop.py
@@ -0,0 +1,28 @@
+#string iteration using For
+name = 'Nobita'
+for i in name:
+    print(i)
+    if(i=='b'):
+        print("it was good")
+
+#list iteration using For
+colors = ['red','blue','green']
+for color in colors:
+    print(color)
+    for j in color:
+        print(j)
+
+#range function
+for k in range(5):
+    print(k)
+
+
+for q in range(1,50):
+    print(q)
+
+for k in range(1,50,5):
+    print(k)
+
+
+  
+    
