99840e8b3bc32a46da350f5e9b4030bbceb9c5b6
Sneha <biradarsneha1245@gmail.com>
2026-01-29 23:30:29 +0530

    slicing

---
+++ slicing.py
@@ -0,0 +1,13 @@
+names = "Nobita"
+print(len(names))
+len1=(len(names))
+print( "nobita is a", len1, "letter of words")
+print(names[0:5]) #uncluding 0 but not 5
+print(names[:])
+print(names[:-3])
+print(names[:len(names)-3])
+print(names[-3:-1])
+
+s="Nobit"
+print(len(s))
+print(s[-4:-2])
